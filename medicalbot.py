13ad76593c802cb47ae21c98bdacf9452844135e
Bilal-Rafique-coder <chbilalrafique2@gmail.com>
2026-07-29 01:10:31 +0500

    changed the model

--- medicalbot.py
+++ medicalbot.py
@@ -21,7 +21,7 @@ def get_vectorstore():
 def load_llm():
     return ChatGroq(
         groq_api_key=os.getenv("GROQ_API_KEY"),
-        model_name="openai/gpt-oss-120b",
+        model_name="llama-3.3-70b-versatile",
         temperature=0.5,
         max_tokens=512
     )
